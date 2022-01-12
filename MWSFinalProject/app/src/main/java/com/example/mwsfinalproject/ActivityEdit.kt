d24f0ebe5920d3c443aeb63c3f1ffd9028282cff
Yoga723 <yogapangestu16@gmail.com>
2022-01-12 15:35:17 +0700

    menambahkan fitur edit data burger

---
+++ MWSFinalProject/app/src/main/java/com/example/mwsfinalproject/ActivityEdit.kt
@@ -0,0 +1,11 @@
+package com.example.mwsfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_edit)
+    }
+}
--- MWSFinalProject/app/src/main/java/com/example/mwsfinalproject/Package_Burger/AdapterBurger.kt
+++ MWSFinalProject/app/src/main/java/com/example/mwsfinalproject/Package_Burger/AdapterBurger.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mwsfinalproject.R
+import com.example.restapiui.API.APIClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AdapterBurger(private val datalist: ArrayList<BurgerModel>) :
     RecyclerView.Adapter<AdapterBurger.ViewHolderData>() {
@@ -22,7 +26,8 @@ class AdapterBurger(private val datalist: ArrayList<BurgerModel>) :
         val tvNamaBurger = itemView.findViewById<TextView>(R.id.tvNamaBurger)
         val tvHargaBurger = itemView.findViewById<TextView>(R.id.tvHargaBurger)
         val tvDeskripsiBurger = itemView.findViewById<TextView>(R.id.tvDeskripsiBurger)
-//        val ivEdit = itemView.findViewById<ImageView>(R.id.ivEdit)
+
+        //        val ivEdit = itemView.findViewById<ImageView>(R.id.ivEdit)
 //        val ivDelete = itemView.findViewById<ImageView>(R.id.ivDelete)
         val context = itemView.context
 
@@ -51,23 +56,78 @@ class AdapterBurger(private val datalist: ArrayList<BurgerModel>) :
         val burgerTerpilih = holder.itemView
 
         burgerTerpilih.setOnClickListener { //Action Saat Recycler view/view terpilih(saat salah satu object terpilih)
-            val dialogView = LayoutInflater.from(ctx).inflate(R.layout.layout_edit, null)
-            val mBuilder = AlertDialog.Builder(ctx).setView(dialogView).setTitle("Select Action")
+            // variabel untuk layout Option
+            val dialogView = LayoutInflater.from(ctx).inflate(R.layout.layout_option, null)
+            val mBuilder = AlertDialog.Builder(ctx).setView(dialogView)
             val alertDialog = mBuilder.show()
-
             val btnEdit = alertDialog.findViewById<Button>(R.id.btnEdit)
             val btnHapus = alertDialog.findViewById<Button>(R.id.btnHapus)
             val btnCancel = alertDialog.findViewById<Button>(R.id.btnCancel)
 
-            btnEdit.setOnClickListener {
 
+            // variabel untuk layout Edit
+            val layoutEdit = LayoutInflater.from(ctx).inflate(R.layout.layout_edit, null)
+            val tvID = layoutEdit.findViewById<TextView>(R.id.tvID)
+            val edProduk = layoutEdit.findViewById<EditText>(R.id.edProduk)
+            val edHarga = layoutEdit.findViewById<EditText>(R.id.edHarga)
+            val edDeskripsi = layoutEdit.findViewById<EditText>(R.id.edDeskripsi)
+
+            val btnSubmit = layoutEdit.findViewById<Button>(R.id.btnSubmit)
+            val btnCancelEdit = layoutEdit.findViewById<Button>(R.id.btnCancel)
+
+            tvID.text = item.ID
+
+            btnEdit.setOnClickListener { // Menampilkan layout edit dalam bentuk alert dialog
+                val mBuilderEdit = AlertDialog.Builder(ctx).setView(layoutEdit).setTitle("Edit")
+                val alertDialogEdit = mBuilderEdit.show()
+
+
+                btnSubmit.setOnClickListener {
+                    val APIClient =
+                        APIClient.create()          /* variabel menghubungi object APIClient dan mengambil fungsi create() */
+
+                    val tvID = (tvID.text as String?)?.toInt()
+                    val edProduk = edProduk.text.toString()
+                    val edHarga = edHarga.text.toString()
+                    val edDeskripsi = edDeskripsi.text.toString()
+
+                    val updateBurger = APIClient.updateBurger(
+                        ID = tvID,
+                        Nama_Burger = edProduk,
+                        Harga = edHarga,
+                        Deskripsi = edDeskripsi,
+                    )
+                    updateBurger.enqueue(object : Callback<BurgerModel> {
+                        override fun onResponse(
+                            call: Call<BurgerModel>,
+                            response: Response<BurgerModel>
+                        ) {
+                            Toast.makeText(ctx, "Data Berhasil Diupdate", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onFailure(call: Call<BurgerModel>, t: Throwable) {
+                            TODO("Not yet implemented")
+                        }
+
+                    })
+
+                    alertDialogEdit.dismiss()
+                    alertDialog.dismiss()
+                }
+
+
+                btnCancelEdit.setOnClickListener {
+                    alertDialogEdit.dismiss()
+                    alertDialog.dismiss()
+                }
             }
 
-            btnCancel.setOnClickListener{
+            btnCancel.setOnClickListener {
                 alertDialog.dismiss()
             }
 
-            Toast.makeText(ctx, "helloasdsadad ${item.ID}", Toast.LENGTH_SHORT).show() // Menampilkan ID dari view yang dipilih
+            Toast.makeText(ctx, "helloasdsadad ${item.ID}", Toast.LENGTH_SHORT)
+                .show() // Menampilkan ID dari view yang dipilih
         }
         holder.tvID.text = item.ID
         holder.tvNamaBurger.text = item.Nama_Burger
